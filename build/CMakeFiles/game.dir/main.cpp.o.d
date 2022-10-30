448e7ff5136db71dac1802ee4f15d1e720e34be7
Oscar <oscar.nordstrom2000@gmail.com>
2022-10-30 18:21:57 +0100

    RM: removed all in build dir

--- build/CMakeFiles/game.dir/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/game.dir/main.cpp.o: \
- /home/oscar/Desktop/Fatty/BTH/Skripting/RaylibCmake/main.cpp \
- /usr/include/stdc-predef.h \
- /home/oscar/Desktop/Fatty/BTH/Skripting/RaylibCmake/external/raylib/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
